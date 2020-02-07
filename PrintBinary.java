846c4974c1e409cf6e3e53a0a238e50e0550b886
Rajath Balasubramanian <rbalasubramanian@mobileiron.com>
2020-02-07 12:48:23 +0530

    Removed license infro

--- PrintBinary.java
+++ PrintBinary.java
@@ -1,18 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2016  Pegasystems Inc.
- * All rights reserved.
- *
- * This  software  has  been  provided pursuant  to  a  License
- * Agreement  containing  restrictions on  its  use.   The  software
- * contains  valuable  trade secrets and proprietary information  of
- * Pegasystems Inc and is protected by  federal   copyright law.  It
- * may  not be copied,  modified,  translated or distributed in  any
- * form or medium,  disclosed to third parties or used in any manner
- * not provided for in  said  License Agreement except with  written
- * authorization from Pegasystems Inc.
-*/
 
 package test.problems;
 
